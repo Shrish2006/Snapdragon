0a00755b378f6b6754f528a60175fec8c64c3b3d
namanch6 <namanchauhan181206@gmail.com>
2026-07-12 10:50:09 +0530

    mq2 test

--- helmet/MQ2_test/MQ2_test.ino
+++ helmet/MQ2_test/MQ2_test.ino
@@ -1,14 +1,14 @@
-#define MQ7_PIN A1
+#define MQ2_PIN A1
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("MQ-7 Sensor Test");
+  Serial.println("MQ-2 Sensor Test");
 }
 
 void loop() {
-  int sensorValue = analogRead(MQ7_PIN);
+  int sensorValue = analogRead(MQ2_PIN);
 
-  Serial.print("MQ-7 Analog Value: ");
+  Serial.print("MQ-2 Analog Value: ");
   Serial.println(sensorValue);
 
   delay(500);
