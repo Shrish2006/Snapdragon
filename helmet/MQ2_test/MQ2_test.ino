51d1cac393d0e2635d37a195718c1185286502da
namanch6 <namanchauhan181206@gmail.com>
2026-07-12 10:51:45 +0530

    mq2 test

--- helmet/MQ2_test/MQ2_test.ino
+++ helmet/MQ2_test/MQ2_test.ino
@@ -1,15 +1,32 @@
-#define MQ2_PIN A1
+// MQ-2 Gas Sensor Test
+
+const int MQ2_PIN = A0;
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("MQ-2 Sensor Test");
+
+  Serial.println("MQ-2 Gas Sensor Test");
+  Serial.println("Warming up sensor...");
+  delay(20000);  // 20 seconds warm-up
 }
 
 void loop() {
   int sensorValue = analogRead(MQ2_PIN);
 
-  Serial.print("MQ-2 Analog Value: ");
+  Serial.print("Gas Sensor Value: ");
   Serial.println(sensorValue);
 
-  delay(500);
+  // Simple indication
+  if (sensorValue < 200) {
+    Serial.println("Air Quality: Clean");
+  }
+  else if (sensorValue < 500) {
+    Serial.println("Air Quality: Moderate");
+  }
+  else {
+    Serial.println("Air Quality: Gas/Smoke Detected!");
+  }
+
+  Serial.println("----------------------");
+  delay(1000);
 }
